fe0b58bffa77c2a336e4bae1866847db5392e93a
smr1004 <138188798+smr1004@users.noreply.github.com>
2023-08-01 16:40:29 +0900

    최초로 커밋

--- src/Option2.java
+++ src/Option2.java
@@ -1,4 +1,5 @@
 public class Option2 extends CoffeeMachine {
+// 2023.8.1(화)
 // 재료(Ingredient) 채우기
 // H2O ,milk ,coffeeBean, cup, money
 //초기값 돈 50000원,H2O 400ml,우유 540ml, 원두 120g, 컵 9개
@@ -27,6 +28,7 @@ public class Option2 extends CoffeeMachine {
         System.out.println("커피머신이 작동했습니다!");
     };
     void 재료출력() {};
+
 //실행 메소드
     public static void main(String[] args) {
 
@@ -59,7 +61,6 @@ public class Option2 extends CoffeeMachine {
                 super(amount);
             }
         }
-
         class Cup extends Option2 {
             public Cup(int amount) {
                 super(amount);
