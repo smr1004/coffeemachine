c26bd5553f703ddbfabd6492ca51302f5d72f059
smr1004 <138188798+smr1004@users.noreply.github.com>
2023-08-03 09:41:55 +0900

    최초로 커밋

--- src/Option2.java
+++ src/Option2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Option2 extends CoffeeMachine {
 // 2023.8.1(화)
 // 재료(Ingredient) 채우기
@@ -9,6 +11,7 @@ public class Option2 extends CoffeeMachine {
     CoffeeBean coffeeBean;
     Cup cup;
     Money money;
+    Ingredient ingredient;
     // 생성자
     public Option2 (Water water, Milk milk, CoffeeBean coffeeBean, Cup cup, Money money) {
         this.water = water;
@@ -16,6 +19,7 @@ public class Option2 extends CoffeeMachine {
         this.coffeeBean = coffeeBean;
         this.cup = cup;
         this.money = money;
+        this.ingredients = new Ingredient[]{this.water,this.milk,this.coffeeBean,this.cup,this.money}
     }
     public Option2(int amount) {
         super();
@@ -26,13 +30,29 @@ public class Option2 extends CoffeeMachine {
     void 가져가기() {};
     void 작동() {
         System.out.println("커피머신이 작동했습니다!");
+    재료출력();
+        while (isRun) {
+            int answer = 사용자입력받기("1.구매 2.채우기 3.가져가기>");
+            switch (answer) {
+case 1 -> buy();
+case 2 -> fill();
+case 3 -> take();
+
+                사용자입력받기();
+            }
+        }
+    }
+    void int 사용자입력받기() {
+        Scanner scanner = new Scanner(System.in) ;
+        System.out.println(message);
+        int input
     };
-    void 재료출력() {};
 
 //실행 메소드
     public static void main(String[] args) {
 
-        CoffeeMachine coffeeMachine = new CoffeeMachine(new Water(amount:400),new Milk(amount:540),
+        int amount;
+        CoffeeMachine coffeeMachine = new CoffeeMachine(new Water(amount:400), new Milk(amount:540),
         new CoffeeBean(amount:120),new Cup(amount:9),new Money(amount:50000));
         coffeeMachine01.작동();
     }
@@ -77,4 +97,6 @@ abstract class Coffee {
     CoffeeBean needCoffeeBean;
     Money price;
 }
+
+
 
